7e2ff16ee31bd91556e18d575ffa1ec66bf307df
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-11-29 10:38:15 +0100

    Fix HHH90003004: firstResult/maxResults specified with collection fetch; applying in memory

--- spring-data-jpa-demo/src/main/java/dns/demo/jpa/repositories/impl/CustomBugRepositoryImpl.java
+++ spring-data-jpa-demo/src/main/java/dns/demo/jpa/repositories/impl/CustomBugRepositoryImpl.java
@@ -12,6 +12,8 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toMap;
+
 @Slf4j
 @Repository
 public class CustomBugRepositoryImpl implements CustomBugRepository {
@@ -24,28 +26,19 @@ public class CustomBugRepositoryImpl implements CustomBugRepository {
 
     /**
      * This is a demo how to deal with MultipleBagFetchException. This is an issue when loading two
-     * @OneToMany relations in the same query. See https://thorben-janssen.com/fix-multiplebagfetchexception-hibernate/.
      *
+     * @OneToMany relations in the same query. See https://thorben-janssen.com/fix-multiplebagfetchexception-hibernate/.
+     * <p>
      * An alternative to deal with MultipleBagFetchException is to use Set instead of List, but
      * we need to be sure the cartesian product (root rows * relation rows) is not large. Otherwise,
      * the overhead of querying the data is considerable.
-     *
+     * <p>
      * In this method we try the approach of separating the queries into two parts.
      */
     @Override
     public List<Bug> findAllWithMultipleOneToManyRelationsJpa(Pageable pageable) {
         log.info("Calling findAllWithMultipleOneToManyRelationsJpa...");
-        List<Bug> bugs = entityManager.createQuery("""
-                        select b from Bug b
-                        join fetch b.status
-                        join fetch b.reportedBy
-                        left join fetch b.assignedTo
-                        left join fetch b.verifiedBy
-                        left join fetch b.screenshots
-                        """, Bug.class)
-                .setFirstResult((int) pageable.getOffset())
-                .setMaxResults(pageable.getPageSize())
-                .getResultList();
+        List<Bug> bugs = getAllBugsWithoutTags(pageable);
 
         final Map<Long, Bug> mapWithTags = entityManager.createQuery("""
                         select distinct b from Bug b
@@ -55,10 +48,58 @@ public class CustomBugRepositoryImpl implements CustomBugRepository {
                 .setParameter("bugs", bugs)
                 .getResultList()
                 .stream()
-                .collect(Collectors.toMap(Bug::getId, Function.identity()));
+                .collect(toMap(Bug::getId, Function.identity()));
 
         // Update the missing data
         bugs.forEach(b -> b.setTags(mapWithTags.get(b.getId()).getTags()));
+
+        return bugs;
+    }
+
+
+    /* This method splits a single query into two to deal with
+     * HHH90003004: firstResult/maxResults specified with collection fetch; applying in memory
+     *  */
+    private List<Bug> getAllBugsWithoutTags(Pageable pageable) {
+        /*
+        ORIGINAL QUERY => return entityManager.createQuery("""
+                        select b from Bug b
+                        join fetch b.status
+                        join fetch b.reportedBy
+                        left join fetch b.assignedTo
+                        left join fetch b.verifiedBy
+                        left join fetch b.screenshots
+                        """, Bug.class)
+                .setFirstResult((int) pageable.getOffset())
+                .setMaxResults(pageable.getPageSize())
+                .getResultList();*/
+
+        // Step 1. Fetch all the bugs without @OneToMany relations.
+        List<Bug> bugs = entityManager.createQuery("""
+                        select b from Bug b
+                        join fetch b.status
+                        join fetch b.reportedBy
+                        left join fetch b.assignedTo
+                        left join fetch b.verifiedBy
+                        """, Bug.class)
+                .setFirstResult((int) pageable.getOffset())
+                .setMaxResults(pageable.getPageSize())
+                .getResultList();
+
+        // Step 2. Fetch the @OneToMany screenshot relation by using the bug ids.
+        List<Long> bugIds = bugs.stream().map(Bug::getId).toList();
+        Map<Long, Bug> bugsWithScreenshotsButNoOtherRelations = entityManager.createQuery("""
+                        select b from Bug b
+                        left join fetch b.screenshots
+                        where b.id in :bugIds
+                        """, Bug.class)
+                .setParameter("bugIds", bugIds)
+                .getResultList()
+                .stream()
+                .collect(toMap(Bug::getId, Function.identity()));
+
+        // Add the screenshots to the initial query
+        bugs.forEach(b -> b.setScreenshots(bugsWithScreenshotsButNoOtherRelations.get(b.getId()).getScreenshots()));
 
         return bugs;
     }
---
+++ spring-data-jpa-demo/src/main/resources/data.sql
@@ -0,0 +1,45 @@
+INSERT INTO Accounts (account_id, account_name, email)
+VALUES (1, 'moe', 'moe@example.com'),
+       (2, 'larry', 'larry@example.com'),
+       (3, 'curly', 'curley@example.com'),
+       (4, 'shemp', 'shemp@example.com');
+
+INSERT INTO Products (product_id, product_name)
+VALUES (1, 'Open RoundFile'),
+       (2, 'Visual TurboBuilder'),
+       (3, 'ReConsider');
+
+INSERT INTO BugStatus (status)
+VALUES ('NEW'),
+       ('IN PROGRESS'),
+       ('CODE COMPLETE'),
+       ('VERIFIED'),
+       ('FIXED'),
+       ('DUPLICATE'),
+       ('WONTFIX');
+
+INSERT INTO Bugs (bug_id, summary, reported_by)
+VALUES (1234, 'crash when I save', 4),
+       (2345, 'increase performance', 3),
+       (3456, 'screen goes blank', 4),
+       (5678, 'unknown conflict between products', 2);
+
+INSERT INTO BugsProducts (bug_id, product_id)
+VALUES (1234, 1),
+       (1234, 3),
+       (3456, 2),
+       (5678, 1),
+       (5678, 3);
+
+INSERT INTO Comments (comment_id, bug_id, author, comment)
+VALUES (6789, 1234, 4, 'It crashes!'),
+       (9876, 2345, 1, 'Great idea!');
+
+INSERT INTO Tags (bug_id, tag)
+VALUES (1234, 'crash'),
+       (1234, 'save'),
+       (1234, 'version-3.0'),
+       (1234, 'windows');
+
+INSERT INTO "screenshots" ("bug_id", "image_id", "screenshot_image", "caption")
+VALUES (1234, 1, 'data', 'Image caption example');
