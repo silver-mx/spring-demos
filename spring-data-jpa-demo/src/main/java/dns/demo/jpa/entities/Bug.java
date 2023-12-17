3565bd236962509d96c46d547b5da8363324c5b1
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-12-18 00:24:43 +0100

    Add equals and hashcode to Bug entity

--- spring-data-jpa-demo/src/main/java/dns/demo/jpa/entities/Bug.java
+++ spring-data-jpa-demo/src/main/java/dns/demo/jpa/entities/Bug.java
@@ -7,10 +7,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -85,4 +87,19 @@ public class Bug {
     @OnDelete(action = OnDeleteAction.NO_ACTION)
     private List<Screenshot> screenshots;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Bug bug = (Bug) o;
+        return getId() != null && Objects.equals(getId(), bug.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
