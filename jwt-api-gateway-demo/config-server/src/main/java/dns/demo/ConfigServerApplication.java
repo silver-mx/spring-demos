0d835c8d172ef8f1e59ca8f35efba63b35210418
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-12-23 18:35:55 +0100

    Small config server changes

--- jwt-api-gateway-demo/config-server/src/main/java/dns/demo/ConfigServerApplication.java
+++ jwt-api-gateway-demo/config-server/src/main/java/dns/demo/ConfigServerApplication.java
@@ -1,14 +1,24 @@
 package dns.demo;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
 
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
+    @Autowired
+    Environment environment;
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+    @PostConstruct
+    void post() {
+        System.out.println("prop=" + environment.getProperty("a.b"));
     }
 }
