611c5498cd17b8bfed27b3c26e618a7ba183e3f4
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-12-27 16:44:58 +0100

    Refactor gateway configuration

--- jwt-api-gateway-demo/gateway-service/src/main/java/dns/demo/gateway/GatewayApplication.java
+++ jwt-api-gateway-demo/gateway-service/src/main/java/dns/demo/gateway/GatewayApplication.java
@@ -1,12 +1,25 @@
 package dns.demo.gateway;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class GatewayApplication {
 
+    @Autowired
+    Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @PostConstruct
+    void post() {
+        log.info("default.config.test=" + environment.getProperty("default.config.test"));
     }
 }
